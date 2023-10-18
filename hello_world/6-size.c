d97d587e8455e456948d644220b4b3c9ce5eb2b3
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-18 16:55:44 +0200

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - A script that prints various data types 
+* Return 0 (Success)
+*/
+
+int main(void)
+{
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+return 0;
+}
