1e4792ac6f0c85dfcabc44cafe3b195171ce4847
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-20 15:36:17 +0200

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,17 @@ int main(void)
 {
 char lowercase = 'a';
 char uppercase = 'A';
-while (lowercase <= 'z' && uppercase <= 'Z')
+
+while (lowercase <= 'z')
 {
 putchar (lowercase);
-putchar (uppercase);
 lowercase++;
-uppercase++;
 }
+while (uppercase <= 'Z')
+{
+putchar (uppercase);
+uppercase++;
+
 putchar ('\n');
 
 return (0);
