2fec7a1076b15e32b4923e5b06a2bbfb6c4bfb04
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-21 09:55:51 +0000

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
  #include <stdio.h>
 /**
-* main -Entry point
-* Return: Always 0 (success)
+*main -Entry point
+*Return: Always 0 (success)
 */
 
 int main(void)
 {
-  int num;
+int num;
 while (num < 10)
 {
 printf("%d, num");
