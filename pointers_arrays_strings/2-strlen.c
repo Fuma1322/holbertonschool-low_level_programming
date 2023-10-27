a6bef67e1610c0e65256deddac572d9d42d687b8
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-27 09:09:16 +0100

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-* int _strlen - returns the length of a string.
+* _strlen - Returns the length of a string
+* @s: Variable being evaluated
 *
-* @s: string
-* Return: returns length
+* Return: Length of string
 */
 int _strlen(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{}
+int i = 0;
+
+while (*s++)
+i++;
 return (i);
 }
