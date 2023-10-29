57b3aec28d10dc439caf87f954f04961d4a52939
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-29 18:04:23 +0200

    Create 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isupper(int c)
+{
+char character = 'A';
+int result = _isupper(character);
+
+if (result)
+{
+printf("%c is an uppercase character.\n", character);
+}
+else
+{
+printf("%c is not an uppercase character.\n", character);
+}
+
+return 0;
+}
