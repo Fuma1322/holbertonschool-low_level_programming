52a88931ab3ebbf1241c4fa92810d68d4250c270
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-29 21:39:31 +0200

    Update 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 
 /**
 * print_numbers - Prints numbers from (0-9).
-*  
+* 
 */
 
 void print_numbers(void)
 {
- int num;
-fot (num = 0; num <= 9; num++)
+int num;
+for(num = 0; num <= 9; num++)
 _putchar((num % 10) + '0');
 _putchar('\n');
 }
