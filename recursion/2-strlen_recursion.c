6ceb57d9dcf91bdb5b35c06918aed61f8b8f4242
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-10-31 13:24:18 +0200

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* _strlen_recursion - Returns the length of a string.
-* @s: The string to be measured.
+* _strlen_recursion - Function that returns the length of a string
+* @s: string to measure
 *
-* Return: The length of the string.
+* Return: Length of the string as int
 */
 int _strlen_recursion(char *s)
 {
 if (*s)
-return (1 + _strlen_recursion((s + 1)))
+return (1 + _strlen_recursion((s + 1)));
 return (0);
 }
