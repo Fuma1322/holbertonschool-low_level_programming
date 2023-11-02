64f0d7268802e9d016eedcebbbe618bfbcd79b40
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-03 00:13:54 +0200

    Update 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,15 +3,15 @@
 /**
 * main - Entry point.
 *@argc: count
-*@argv: argument string 
+*@argv: argument string
 *
 * Return: int count of arguments
 */
 
 int main(int argc, char *argv[])
 {
-// Print the value of argc, which represents the number of arguments
+/* Print the value of argc, which represents the number of arguments */
 printf("%d\n", argc - 1);
 
-return 0;
+return (0);
 }
