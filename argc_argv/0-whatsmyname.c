aec23320b9d18bc8ed7ff8281d49c5cd42790f74
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-02 21:11:10 +0200

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main - Entry point of the program.
+* It prints the name of the program, including the path.
+*/
+
 int main(int argc, char *argv[])
 {
 if (argc > 0 && argv[0] != NULL)
