f755884c915205b544ee7b0148671d17b871c1c1
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-03 11:22:31 +0200

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+* main - multiplies two numbers
+* @argc: counts
+* @argv: string array for the argument
+* Return: always 0 else 1
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+/* Check if the program received exactly two arguments*/
+printf("Error\n");
+return 1;
+}
+/* Convert the command-line arguments to integers */
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2);
+
+/* Multiply the two numbers */
+int result = num1 * num2;
+
+/* Print the result followed by a new line */
+printf("%d\n", result);
+return (0);
+}
