0734a2f9b66602f12584e944025484f0e90ee749
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-03 12:15:50 +0200

    Update 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+* main - adds positive numbers. 
+*@argc: counts
+*@argv: string array for the arguments
+*
+* Return: 0 else 1
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+{
+/* No numbers provided, print 0 and return 0 */
+printf("0\n");
+return (0);
+}
+
+int sum = 0;
+
+for (int i = 1; i < argc; i++)
+{
+int number = atoi(argv[i]);
+
+if (number <= 0)
+{
+/* Non-positive numbers are not allowed, print "Error" and return 1 */
+printf("Error\n")
+return 1;
+}
+
+sum += number;
+}
+
+/* Print the sum of positive numbers followed by a new line */
+printf("%d\n", sum);
+
+return (0);
+}
