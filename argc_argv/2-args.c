40cc2d161827ac76376d0f394785008318bd6601
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-03 10:37:55 +0200

    Update 2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+* main - Entry point.
+*@argc: count
+*@argv: string with arguments
+*
+* Return: 0 (success)
+
+int main(int argc, char *argv[])
+{
+/* Loop through the arguments and print each one on a new line*/
+for (int i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+return (0);
+}
