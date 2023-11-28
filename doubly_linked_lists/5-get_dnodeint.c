eea9f49bbd04683ca1bd2d41453b25677a3b8f5c
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-28 21:50:46 +0200

    Coded 21:50

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* get_dnodeint_at_index - Returns the nth node of a dlistint_t.
+* get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
 *@head: pointer to head of list.
 *@index: index of the node, starting from 0.
 *
