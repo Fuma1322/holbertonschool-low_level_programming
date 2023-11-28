9775cfd55a00621848c5458e545015a7de6e579c
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-28 17:24:17 +0200

    Coded 17:24

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,1 +1,28 @@
+#include "lists.h"
 
+/**
+*
+*
+*
+*
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlist_t *stash_node;
+
+stash_node = malloc(sizeof(dlistint_t));
+
+if (stash_node == NULL)
+return (NULL);
+
+stash_node->n = n;
+stash_node->prev = NULL;
+stash_node->next = *head;
+
+if (*head != NULL)
+(*head)->prev = stash_node;
+*head = stash_node;
+
+return (stash_node);
+}
