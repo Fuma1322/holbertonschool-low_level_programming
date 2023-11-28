6cff258a8cc47a4e175f3f031dc1fdaf286993c3
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-28 17:28:18 +0200

    Coded 17:28

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
+* add_dnodeint - Adds a new node at the beggining of a dlistint_t list.
+* @head: pointer to the pointer of the head of the list.
+* @n: interger to be stored in the stash node.
 *
-*
-*
-*
+* Return: address of the new element, or NULL if it failed.
 */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
