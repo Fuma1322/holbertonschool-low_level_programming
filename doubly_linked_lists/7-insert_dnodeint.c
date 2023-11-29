6505df053490103844a9fd021a4f4de5fe58e92b
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-29 12:42:41 +0200

    Coded 12:42

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -50,7 +50,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 if (h == NULL)
 return (NULL);
 
-dlistint_t *temp = *h;
+dlistint_t *temp;
 unsigned int i = 0;
 
 if (idx == 0)
