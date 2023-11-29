cf6ab0969d9c1ff9e73eacb60ae674aeba2e9d6b
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-29 11:34:52 +0200

    Coded 11:35

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,63 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position.
-* @h: Pointer to the pointer of head of the list.
-* @idx: index where the new node should be added.
+* create_dnode - creates a new doubly linked list node.
 * @n: integer to be stored in the new node.
-*
 * Return: Address of the new node, or NULL if it failed.
 */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *create_dnode(int n)
 {
-dlistint_t *new_node, *temp;
-unsigned int i = 0;
-
-if (h == NULL)
-return (NULL);
-
-new_node = malloc(sizeof(dlistint_t));
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 
 new_node->n = n;
-
-if (idx == 0)
-{
 new_node->prev = NULL;
-new_node->next = *h;
-if (*h != NULL)
-(*h)->prev = new_node;
-*h = new_node;
+new_node->next = NULL;
+
 return (new_node);
 }
 
-temp = *h;
+/**
+* insert_dnode_at_beginning - inserts a new node at the beginning of the list.
+* @h: Pointer to the pointer of head of the list.
+* @new_node: Pointer to the new node.
+* Return: Address of the new node, or NULL if it failed.
+*/
+dlistint_t *insert_dnode_at_beginning(dlistint_t **h, dlistint_t *new_node)
+{
+if (new_node == NULL)
+return (NULL);
+
+new_node->next = *h;
+if (*h != NULL)
+(*h)->prev = new_node;
+
+*h = new_node;
+
+return (new_node);
+}
+
+/**
+* insert_dnodeint_at_index - inserts a new node at a given position.
+* @h: Pointer to the pointer of head of the list.
+* @idx: index where the new node should be added.
+* @n: integer to be stored in the new node.
+* Return: Address of the new node, or NULL if it failed.
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+if (h == NULL)
+return (NULL);
+
+if (idx == 0) {
+dlistint_t *new_node = create_dnode(n);
+return insert_dnode_at_beginning(h, new_node);
+}
+
+dlistint_t *temp = *h;
+unsigned int i = 0;
+
 while (temp != NULL && i < idx - 1)
 {
 temp = temp->next;
@@ -40,10 +65,11 @@ i++;
 }
 
 if (temp == NULL)
-{
-free(new_node);
-return (NULL);
-}
+return NULL;
+
+dlistint_t *new_node = create_dnode(n);
+if (new_node == NULL)
+return NULL;
 
 new_node->prev = temp;
 new_node->next = temp->next;
@@ -53,5 +79,5 @@ temp->next->prev = new_node;
 
 temp->next = new_node;
 
-return (new_node);
+return new_node;
 }
