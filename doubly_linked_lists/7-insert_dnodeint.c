6e4633be7ef16f7024715c58eb42841d83a839a7
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-11-29 13:06:23 +0200

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,87 +2,85 @@
 
 /**
 * create_dnode - creates a new doubly linked list node.
-* @n: integer to be stored in the new node.
-* Return: Address of the new node, or NULL if it failed.
+* @n: integer to be stored in the lebo node.
+* Return: Address of the lebo node, or NULL if it failed.
 */
 dlistint_t *create_dnode(int n)
 {
-dlistint_t *new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
+dlistint_t *lebo_node = malloc(sizeof(dlistint_t));
+if (lebo_node == NULL)
 return (NULL);
 
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = NULL;
+lebo_node->n = n;
+lebo_node->prev = NULL;
+lebo_node->next = NULL;
 
-return (new_node);
+return (lebo_node);
 }
 
 /**
-* insert_dnode_at_beginning - inserts a new node at the beginning of the list.
+* insert_dnode_at_beginning - inserts a lebo node at the beginning of the list.
 * @h: Pointer to the pointer of head of the list.
-* @new_node: Pointer to the new node.
-* Return: Address of the new node, or NULL if it failed.
+* @lebo_node: Pointer to the lebo node.
+* Return: Address of the lebo node, or NULL if it failed.
 */
-dlistint_t *insert_dnode_at_beginning(dlistint_t **h, dlistint_t *new_node)
+dlistint_t *insert_dnode_at_beginning(dlistint_t **h, dlistint_t *lebo_node)
 {
-if (new_node == NULL)
+if (lebo_node == NULL)
 return (NULL);
 
-new_node->next = *h;
+lebo_node->next = *h;
 if (*h != NULL)
-(*h)->prev = new_node;
+(*h)->prev = lebo_node;
 
-*h = new_node;
+*h = lebo_node;
 
-return (new_node);
+return (lebo_node);
 }
 
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position.
+* insert_dnodeint_at_index - inserts a lebo node at a given position.
 * @h: Pointer to the pointer of head of the list.
-* @idx: index where the new node should be added.
-* @n: integer to be stored in the new node.
-* Return: Address of the new node, or NULL if it failed.
+* @idx: index where the lebo node should be added.
+* @n: integer to be stored in the lebo node.
+* Return: Address of the lebo node, or NULL if it failed.
 */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 if (h == NULL)
 return (NULL);
 
-dlistint_t *temp;
-unsigned int i = 0;
+dlistint_t *hot = *h;
+unsigned int i;
 
 if (idx == 0)
 {
-dlistint_t *new_node = create_dnode(n);
-return (insert_dnode_at_beginning(h, new_node));
+dlistint_t *lebo_node = create_dnode(n);
+return (insert_dnode_at_beginning(h, lebo_node));
 }
 
-temp = *h;
-  
-while (temp != NULL && i < idx - 1)
+for (i = 0; hot != NULL && i < idx - 1; i++)
 {
-temp = temp->next;
-i++;
+hot = hot->next;
 }
 
-if (temp == NULL)
+if (hot == NULL)
 return (NULL);
 
 {
-dlistint_t *new_node = create_dnode(n);
-if (new_node == NULL)
+dlistint_t *lebo_node = create_dnode(n);
+if (lebo_node == NULL)
 return (NULL);
 
-new_node->prev = temp;
-new_node->next = temp->next;
+lebo_node->prev = hot;
+lebo_node->next = hot->next;
 
-if (temp->next != NULL)
-temp->next->prev = new_node;
+if (hot->next != NULL)
+hot->next->prev = lebo_node;
 
-temp->next = new_node;
+hot->next = lebo_node;
 
-return (new_node);
+return (lebo_node);
 }
 }
