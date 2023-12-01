e1bcd5f3b51e36f7beb085c4ace54313c5ae780c
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-01 11:19:23 +0200

    Coded 11:20

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include "main.h"
 
+/**
+* clear_bit - Sets the value of a bit to 0 at a given index.
+* @n: Pointer to the number whose bit needs to be cleared.
+* @index: The index of the bit to be cleared, starting from 0.
+* Return: 1 if it worked, or -1 if an error occurred.
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index) {
+if (index >= sizeof(unsigned long int) * 8)
+return -1;
+
+*n = *n & ~(1UL << index);
+return 1;
+}
+
+int main() {
+unsigned long int num = 98;
+int result;
+
+printf("%lu\n", num);
+
+result = clear_bit(&num, 5);
+
+if (result == 1)
+{
+printf("%lu\n", num);
+} 
+else
+{
+printf("Error occurred\n");
+}
+
+result = clear_bit(&num, 1);
+
+if (result == 1) {
+printf("%lu\n", num);
+} 
+else
+{
+printf("Error occurred\n");
+}
+
+return 0;
+}
