81a3d1a42723af7c583bdcfcfb7204c3bf1442d4
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-01 11:22:10 +0200

    Coded 11:22

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,40 +7,11 @@
 * Return: 1 if it worked, or -1 if an error occurred.
 */
 
-int clear_bit(unsigned long int *n, unsigned int index) {
+int clear_bit(unsigned long int *n, unsigned int index)
+{
 if (index >= sizeof(unsigned long int) * 8)
 return -1;
 
 *n = *n & ~(1UL << index);
-return 1;
-}
-
-int main() {
-unsigned long int num = 98;
-int result;
-
-printf("%lu\n", num);
-
-result = clear_bit(&num, 5);
-
-if (result == 1)
-{
-printf("%lu\n", num);
-} 
-else
-{
-printf("Error occurred\n");
-}
-
-result = clear_bit(&num, 1);
-
-if (result == 1) {
-printf("%lu\n", num);
-} 
-else
-{
-printf("Error occurred\n");
-}
-
-return 0;
+return (1);
 }
