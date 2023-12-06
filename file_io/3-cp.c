7b8e9d8b153560a9b8cb433c2919a7f95d7c2ede
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-07 00:12:28 +0200

    Coded 00:12

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,71 +7,52 @@
 
 #define BUFFER_SIZE 1024
 
-/**
-* main - Entry point
-* @argc: The number of command line arguments
-* @argv: The command line arguments
-*
-* Return: 0 on success, or one of 97, 98, 99, 100 on error
-*/
-
-int main(int argc, char *argv[])
-{
-int fd_from, fd_to, read_bytes, write_bytes;
-char buffer[BUFFER_SIZE];
-
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+void print_error(char *message, char *filename, int exit_code) {
+    dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
+    exit(exit_code);
 }
 
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+int open_file(char *filename, int flags, mode_t mode) {
+    int fd = open(filename, flags, mode);
+    if (fd == -1) {
+        print_error("Can't open", filename, 98);
+    }
+    return fd;
 }
 
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-exit(99);
+void copy_file(int fd_from, int fd_to) {
+    char buffer[BUFFER_SIZE];
+    ssize_t read_bytes, write_bytes;
+
+    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+        write_bytes = write(fd_to, buffer, read_bytes);
+        if (write_bytes != read_bytes) {
+            print_error("Can't write to", "destination file", 99);
+        }
+    }
+
+    if (read_bytes == -1) {
+        print_error("Can't read from", "source file", 98);
+    }
 }
 
-while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-{
-write_bytes = write(fd_to, buffer, read_bytes);
-if (write_bytes != read_bytes)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-close(fd_to);
-exit(99);
-}
-}
+int main(int argc, char *argv[]) {
+    int fd_from, fd_to;
 
-if (read_bytes == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(fd_from);
-close(fd_to);
-exit(98);
-}
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
-}
+    fd_from = open_file(argv[1], O_RDONLY, 0);
+    fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-exit(100);
-}
+    copy_file(fd_from, fd_to);
 
-return (0);
+    if (close(fd_from) == -1 || close(fd_to) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
+        exit(100);
+    }
+
+    return 0;
 }
