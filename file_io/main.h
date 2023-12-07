c9c62923c940f3543efd999d6fcf83b8a59f8586
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-07 12:19:32 +0200

    test

--- file_io/main.h
+++ file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /* FUNCTION prototypes*/
 int append_text_to_file(const char *filename, char *text_content);
