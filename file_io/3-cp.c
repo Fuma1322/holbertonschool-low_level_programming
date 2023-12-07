e0dd468b72472d2a04a37a2b39c54bc7c65d2f64
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-07 12:17:27 +0200

    Coded 12:17

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,7 +31,6 @@ if (written == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-
 writeCheck = write(fd2, buf, written);
 if (writeCheck == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
