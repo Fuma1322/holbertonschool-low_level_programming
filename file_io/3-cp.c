b7a3e54da0a5986254860f1746d34f1312105e37
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-07 11:51:29 +0200

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,35 +9,36 @@
 
 void print_error(char *message, char *filename, int exit_code)
 {
-    dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
-    exit(exit_code);
+dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
+exit(exit_code);
 }
 
 int open_file(char *filename, int flags, mode_t mode)
 {
-    int fd = open(filename, flags, mode);
-    if (fd == -1) {
-        print_error("Can't write to", filename, 99);
-    }
-    return (fd);
+int fd = open(filename, flags, mode);
+if (fd == -1) {
+print_error("Can't write to", filename, 99);
+}
+return (fd);
 }
 
 void copy_file(int fd_from, int fd_to)
 {
-    char buffer[BUFFER_SIZE];
-    ssize_t read_bytes, write_bytes;
+char buffer[BUFFER_SIZE];
+ssize_t read_bytes, write_bytes;
 
-    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        write_bytes = write(fd_to, buffer, read_bytes);
-        if (write_bytes != read_bytes) {
-            print_error("Can't write to", "destination file", 99);
+while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+write_bytes = write(fd_to, buffer, read_bytes);
+if (write_bytes != read_bytes)
+{
+print_error("Can't write to", "destination file", 99);
 }
 }
 
 if (read_bytes == -1)
 {
-print_error("Can't read from", "test_folder/textfile_0", 98);
+print_error("Can't read from", "source file", 98);
 }
 }
 
@@ -49,18 +50,18 @@ if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
-    }
+}
 
-    fd_from = open_file(argv[1], O_RDONLY, 0);
-    fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+fd_from = open_file(argv[1], O_RDONLY, 0);
+fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-    copy_file(fd_from, fd_to);
+copy_file(fd_from, fd_to);
 
-    if (close(fd_from) == -1 || close(fd_to) == -1) 
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd descriptors\n");
-        exit(100);
-    }
+if (close(fd_from) == -1 || close(fd_to) == -1) 
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd descriptors\n");
+exit(100);
+}
 
-    return 0;
+return (0);
 }
