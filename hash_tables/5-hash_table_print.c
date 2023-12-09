8def440626f50377cd53f38cb16c6fcd0d87b215
Fuma1322 <146758442+Fuma1322@users.noreply.github.com>
2023-12-09 10:35:39 +0200

    Coded 11:45

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,1 +1,32 @@
+#include "hash_tables.h"
 
+/**
+ * hash_table_print - Prints the keys and values.
+ *@ht: Pointer to the table.
+ *
+ * Return: none
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *tmp;
+char *sp;
+
+if (ht == NULL)
+return;
+
+printf("{");
+sp = "";
+
+for (i = 0; i < ht->size; i++)
+{
+tmp = ht->array[i];
+while (tmp != NULL)
+{
+printf("%s'%s': '%s'", sp, tmp->key, tmp->value);
+sp = ", ";
+tmp = tmp->next;
+}
+}
+printf("}\n");
+}
